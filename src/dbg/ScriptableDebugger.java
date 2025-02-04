6953ca16cbd95421a179b5b9bc8ebcf8b7428dcf
abdelaziz-amil <abdelaziz.amil@etudiant.univ-brest.fr>
2025-02-04 12:22:59 +0100

    initial commit qui marche

---
+++ src/dbg/ScriptableDebugger.java
@@ -0,0 +1,123 @@
+package dbg;
+
+import com.sun.jdi.*;
+import com.sun.jdi.connect.Connector;
+import com.sun.jdi.connect.IllegalConnectorArgumentsException;
+import com.sun.jdi.connect.LaunchingConnector;
+import com.sun.jdi.connect.VMStartException;
+import com.sun.jdi.event.*;
+import com.sun.jdi.request.BreakpointRequest;
+import com.sun.jdi.request.ClassPrepareRequest;
+import com.sun.jdi.request.StepRequest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Map;
+
+public class ScriptableDebugger {
+
+    private Class debugClass;
+    private VirtualMachine vm;
+    private boolean autoStepping = false;
+
+    public VirtualMachine connectAndLaunchVM() throws IOException, IllegalConnectorArgumentsException, VMStartException {
+        LaunchingConnector launchingConnector = Bootstrap.virtualMachineManager().defaultConnector();
+        Map<String, Connector.Argument> arguments = launchingConnector.defaultArguments();
+        arguments.get("main").setValue(debugClass.getName());
+        return launchingConnector.launch(arguments);
+    }
+
+    public void attachTo(Class debuggeeClass) {
+        this.debugClass = debuggeeClass;
+        try {
+            vm = connectAndLaunchVM();
+            enableClassPrepareRequest(vm);
+            startDebugger();
+        } catch (IOException | IllegalConnectorArgumentsException | VMStartException e) {
+            e.printStackTrace();
+            System.out.println(e.toString());
+        } catch (VMDisconnectedException e) {
+            System.out.println("Virtual Machine is disconnected: " + e.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void enableClassPrepareRequest(VirtualMachine vm) {
+        ClassPrepareRequest classPrepareRequest = vm.eventRequestManager().createClassPrepareRequest();
+        classPrepareRequest.addClassFilter(debugClass.getName());
+        classPrepareRequest.enable();
+    }
+
+    public void setBreakPoint(String className, int lineNumber) throws AbsentInformationException {
+        for (ReferenceType targetClass : vm.allClasses()) {
+            if (targetClass.name().equals(className)) {
+                Location location = targetClass.locationsOfLine(lineNumber).get(0);
+                BreakpointRequest bpReq = vm.eventRequestManager().createBreakpointRequest(location);
+                bpReq.enable();
+            }
+        }
+    }
+
+    public void enableStepRequest(LocatableEvent event) {
+        // Supprime les StepRequest existants pour ce thread
+        vm.eventRequestManager().deleteEventRequests(vm.eventRequestManager().stepRequests());
+
+        StepRequest stepRequest = vm.eventRequestManager()
+                .createStepRequest(event.thread(), StepRequest.STEP_MIN, StepRequest.STEP_OVER);
+        stepRequest.enable();
+    }
+
+    private void waitForUserInput(LocatableEvent event) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Debugger paused. Type 'step' to continue stepping or press any touch to end the program : ");
+        try {
+            String input = reader.readLine();
+            if ("step".equalsIgnoreCase(input)) {
+                autoStepping = false;
+                enableStepRequest(event);
+            } else {
+                autoStepping = true;
+                enableStepRequest(event);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void startDebugger() throws VMDisconnectedException, InterruptedException, AbsentInformationException {
+        EventSet eventSet;
+        while ((eventSet = vm.eventQueue().remove()) != null) {
+            for (Event event : eventSet) {
+                System.out.println(event.toString());
+
+                if (event instanceof ClassPrepareEvent) {
+                    setBreakPoint(debugClass.getName(), 6);
+                }
+
+                if (event instanceof BreakpointEvent || event instanceof StepEvent) {
+                    if (!autoStepping) {
+                        waitForUserInput((LocatableEvent) event);
+                    } else {
+                        enableStepRequest((LocatableEvent) event);
+                    }
+                }
+
+                if (event instanceof VMDisconnectEvent) {
+                    System.out.println("End of program");
+                    InputStreamReader reader = new InputStreamReader(vm.process().getInputStream());
+                    OutputStreamWriter writer = new OutputStreamWriter(System.out);
+                    try {
+                        reader.transferTo(writer);
+                        writer.flush();
+                    } catch (IOException e) {
+                        System.out.println("Target VM input stream reading error.");
+                    }
+                }
+                vm.resume();
+            }
+        }
+    }
+}
---
+++ src/dbg/command/Sender.java
@@ -0,0 +1,4 @@
+package dbg.command;
+
+public class Sender {
+}
